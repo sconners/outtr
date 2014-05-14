8aefa8ecf2f8c08f195d2f7a8a24f667d3ac6d41
Shawn Conners <sconners@kissyfaceinteractive.com>
2014-05-14 17:26:08 -0600

    Outtr Lockers and API

--- index.php
+++ index.php
@@ -1,38 +1,35 @@
+<?php
+	ini_set('error_reporting', E_ALL);
+?>
 <html>
 	<head>
-		<title>outtr</title>
+		<title>Outtr</title>
 		<script>
 			function funStartup(){
-				// Placeholder function for page startup
+				// Placeholder function for page javascript startup
 			}
 		</script>
 		<style>
-			p, li{
+			p, li, table, tr, td{
 				margin: 0px;
 				padding: 8px;
 			}
-			#divBackground{
-				position: absolute;
-				left: 0px;
-				top: 0px;
-				margin: 0px;
-				padding: 0px;
-				width: 100%;
-				height: 100%;
-				background-color: #444;
+			table, tr, td{
+				border: 1px solid black;
 			}
 			#divHowLockersWork{
-				position: absolute;
-				left: 32px;
-				top: 32px;
 				margin: 0px;
 				padding: 16px;
 				background-color: #DDD;
 			}
+			#divLockersAPI{
+				margin: 0px;
+				padding: 16px;
+				background-color: #EEE;
+			}
 		</style>
 	</head>
 	<body onload="funStartup();">
-		<div id="divBackground"></div>
 		<div id="divHowLockersWork">
 			<h3>How Outtr Lockers Work</h3>
 			<p>The following happens over SSL.</p>
@@ -43,7 +40,51 @@
 				<li>Client verifies that the locker has everything.</li>
 				<li>Client locks locker.</li>
 			</ol>
-			<p>
+		</div>
+		<div id="divLockersAPI">
+			<h3>Lockers Server API</h3>
+			<p>The following happens over SSL. All content is base64 encoded.</p>
+			<table>
+				<tr>
+					<td>Action</td>
+					<td>URL</td>
+					<td>Input</td>
+					<td>Output</td>
+				</tr>
+				<tr>
+					<td>Interface (default)</td>
+					<td>index.php</td>
+					<td>NA</td>
+					<td>HTML/CSS/JS for interacting with Outtr.</td>
+				</tr>
+				<tr>
+					<td>New Locker</td>
+					<td>locker_new.php</td>
+					<td>NA</td>
+					<td>32 bit character string.<br/>(example: RRermiw8620Uonnw3WOfqqq90711F0xz)</td>
+				</tr>
+				<tr>
+					<td>Add to Locker</td>
+					<td>locker_add.php</td>
+					<td>
+						Locker Key.<br/>(locker_key=RRermiw8620Uonnw3WOfqqq90711F0xz)<br/>
+						Locker Content.<br/>(locker_content=this_is_a_base64_encoded_string)
+					</td>
+					<td>NA</td>
+				</tr>
+				<tr>
+					<td>Read Locker</td>
+					<td>locker_read.php</td>
+					<td>Locker Key.<br/>(locker_key=RRermiw8620Uonnw3WOfqqq90711F0xz)</td>
+					<td>Locker Content<br/>(this_is_a_base64_encoded_string)</td>
+				</tr>
+				<tr>
+					<td>Lock Locker</td>
+					<td>locker_lock.php</td>
+					<td>Locker Key.<br/>(locker_key=RRermiw8620Uonnw3WOfqqq90711F0xz)</td>
+					<td></td>
+				</tr>
+			</table>
 		</div>
 	</body>
 </html>
